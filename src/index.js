98ea2ae98be3dec2a827c21ec6ba8294da1f6c6b
doodleman218 <dcwolff218@gmail.com>
2020-05-27 09:13:38 -0500

    David evening work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const beerCard = document.querySelector(".card")
 let beerArray = []
-let chugList = []
+
 let chuckList = []
 
 
@@ -15,24 +15,42 @@ function pushToBeerArray(beer){
 }
 
 function renderBeer(beer) {
-
-  const { brand, name, location, image, uploader_comment, chugs, chucks } = beer[0].attributes
+ 
+  const { brand, name, location, image, uploader_comment, chugs, chucks, comments } = beer[0].attributes
   beerCard.innerHTML =
   `<h2>${brand}</h2>
-  <h3>${name}</h3>
-  <h3>${location}</h3>
-  
+  <h3>Bartender: ${name}</h3>
+  <h3>Location: ${location}</h3>
   <img src=${image} class="beer-avatar" />
   <h4>${uploader_comment}</h4> 
   <ul id="user_comments"></ul>
+  <form class="add-comment-form>
+  <input
+  type="text"
+  name="comment"
+  value=""
+  placeholder="Comment"
+  class="input-text"
+  />
+  <br>
+  <input
+  type="submit"
+  name="submit"
+  value="Add Comment"
+  class="submit"
+  />
+  </form>
   <span>
   <p class="chugLikes"> ${chugs} Chugs</p>
   <button class="chug-btn" data-id=${beer[0].id}>Chug </button>
   <p class="chuckLikes"> ${chucks} Chucks</p>
   <button class="chuck-btn" data-id=${beer[0].id}>Chuck</button>
   </span>`
-  // console.log(name)
-  // console.log(beer[0].id)
+  const userComments = document.getElementById("user_comments")
+  comments.forEach(comment => {
+    userComments.innerHTML += `<li>-${comment.text}</li>`
+  })
+
 }
 
 function addChug() {
@@ -57,24 +75,23 @@ function addChug() {
       .then(resp => resp.json())
       .then(chug => event.target.previousElementSibling.innerHTML = `${currentChugs + 1} Chugs`)
     
-
+      const chugList = []
       let chugItem = beerArray.shift()
       chugList.push(chugItem)
       renderBeer(beerArray)
       loadChugList(chugList)
-      console.log(chugList)
-
     }
   })
 }
 const chugContainer = document.querySelector("#chug-list-container")
 
 function loadChugList(chugList) {
+  const chugListItems = document.getElementById("chug-list-items")
   chugList.forEach(beer => {
-    chugContainer.innerHTML += `${beer.attributes.brand}`
+    console.log(beer.id)
+    chugListItems.innerHTML += `<li class="chug-list-beers" data-beer-id=${beer.id}>${beer.attributes.brand}</li>` 
   }
   )
-
 }
 
 function addChuck() {
